b447e05088531def4b9f9eb875521ee0cb26e2bb
khanhcan2k4 <levietkhanh2k4@gmail.com>
2026-01-09 15:46:28 +0700

    feat: test print function

--- app.ts
+++ app.ts
@@ -1,12 +1,33 @@
 import { SelectionHelper } from "./src/helpers/selection.helper";
+import inputData from "./data/input-data.json";
+import { join } from "path";
+import testCases from "./data/testcases.json";
+import { TestCase } from "./src/models/testcase.model";
 
-class App {
-    constructor() {
-        // const csv = new CSV();
-        // console.log(csv.ReadCSV('./data/random_levels.csv'));
-        const selection = new SelectionHelper().convertCsvToSelection('./data/testcases.csv');
-        console.log(selection);
-    }
+const selectionHelper = new SelectionHelper();
+
+const selectionPath = join(
+  process.cwd(),
+  inputData.targetFolder,
+  inputData.targetTestCaseFileName + ".csv"
+);
+const selections = selectionHelper.convertCsvToSelection(
+  "./data/select_testcase.csv"
+);
+
+const checkedSelections = selectionHelper.getCheckedSelectionsByLevel(
+  selections,
+  inputData.filterLevel
+);
+
+const _testCases = testCases.map((tc: any) => {
+  const testCase = new TestCase();
+  testCase.id = tc.id;
+  testCase.name = tc.name;
+  testCase.content = tc.content;
+  return testCase;
+});
+
+for (const testCase of _testCases) {
+  console.log(testCase.toString());
 }
-
-new App();
