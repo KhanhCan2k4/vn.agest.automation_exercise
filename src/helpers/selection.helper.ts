76691a16df2ad1e48f68ce1eb8a9f3c6f935ba30
Tn Lc_ <lamtanloc3@gmail.com>
2026-01-09 14:10:17 +0700

    add getCheckedSelectionsByLevel and getTestcaseByLevel function

--- src/helpers/selection.helper.ts
+++ src/helpers/selection.helper.ts
@@ -1,9 +1,10 @@
 import {readFileSync} from 'fs';
 import { parse } from "csv-parse/sync";
 import { Selection } from "../models/selection.model";
+import inputData from "../../data/input-data.json";
+import { TestCase } from '../models/testcase.model';
 
-export class CSV {
-    constructor() {}
+export class SelectionHelper {
 
     convertCsvToSelection(filePath: string): Selection[] {
 
@@ -18,21 +19,36 @@ export class CSV {
         const selections: Selection[] = [];
 
         for (const row of csvRows) {
-            const testcaseId: string = row["id"];
+            const testcaseId: string = row[inputData.mainTitle];
 
             for (const [levelName, isChecked] of Object.entries(row)) {
-                if (levelName === "id") continue;
-
-                selections.push(
-                    new Selection(
-                        testcaseId,
-                        levelName,
-                        isChecked === "X"
-                    )
-                );
+                if (levelName !== inputData.mainTitle){
+                    selections.push(
+                        new Selection(
+                            testcaseId,
+                            levelName,
+                            isChecked == inputData.selectSymbol
+                        )
+                    );
+                }
+                
             }
         }
         return selections;
+    }
+
+    getCheckedSelectionsByLevel(selections: Selection[], levelName: string): Selection[] {
+        return selections.filter((selection) =>
+            selection.level === levelName && selection.isSelected
+        );
+    }
+    
+    getTestcaseByLevel(testcases: TestCase[],selections: Selection[], levelName: string): TestCase[] {
+        const checkedSelections = this.getCheckedSelectionsByLevel(selections, levelName);
+        const testcaseIds = checkedSelections.map(selection => selection.testcaseID);
+        return testcases.filter(testcase =>
+            testcaseIds.includes(testcase.id)
+        );
     }
 }
 
