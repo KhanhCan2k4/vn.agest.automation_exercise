74301cde38642219b55bffcc969ea8f5d81ec0ec
khanh-viet-le <khanh.viet.le@agest.vn>
2026-01-12 14:27:10 +0700

    chore: refactor test case tracking to iterate over multiple cases

--- tests/testcases.spec.ts
+++ tests/testcases.spec.ts
@@ -3,23 +3,25 @@ import inputData from "../data/input-data.json";
 import { TestCasesPage } from "../src/pages/testcases.page";
 import { FileHelper } from "../src/helpers/file.helper";
 
-test("Track testcases", async ({ page }) => {
-  const testCasePage = new TestCasesPage(page);
-  await testCasePage.goto();
+for (let i = 0; i < 5; i++) {
+  test(`Track testcases (${i + 1})`, async ({ page }) => {
+    const testCasePage = new TestCasesPage(page);
+    await testCasePage.goto();
 
-  const testCaseList = await testCasePage.getTestCaseList();
+    const testCaseList = await testCasePage.getTestCaseList();
 
-  const fileHelper = new FileHelper(inputData.targetFolder);
+    const fileHelper = new FileHelper(inputData.targetFolder);
 
-  fileHelper.writeToFile(
-    inputData.targetTestCaseFileName + ".json",
-    testCaseList
-  );
+    fileHelper.writeToFile(
+      inputData.targetTestCaseFileName + ".json",
+      testCaseList
+    );
 
-  fileHelper.writeToFile(inputData.targetTestCaseFileName + ".csv", [
-    [inputData.mainTitle, ...inputData.levels],
-    ...testCaseList.map((tc) => [tc.id]),
-  ]);
+    fileHelper.writeToFile(inputData.targetTestCaseFileName + ".csv", [
+      [inputData.mainTitle, ...inputData.levels],
+      ...testCaseList.map((tc) => [tc.id]),
+    ]);
 
-  expect(testCaseList.length).toBeGreaterThan(0);
-});
+    expect(testCaseList.length).toBeGreaterThan(0);
+  });
+}
